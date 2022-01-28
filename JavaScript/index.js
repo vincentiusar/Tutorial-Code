59e2b2457b9d20f400eea4e0fe1db60d15081d95
vincentiusar <vincentiusarnoldfridolin@gmail.com>
2022-01-28 13:49:16 +0700

    Basic 1

---
+++ JavaScript/index.js
@@ -0,0 +1,26 @@
+var x = 2, y = 10;
+console.log("x = " + x, "y = " + y);
+console.log("x == y");
+console.log(x == y);
+
+console.log("x % y");
+console.log(x % y);
+
+var str = "apa saya lupa";
+console.log(str);
+console.log("str.length");
+console.log(str.length);
+
+console.log("\"10\" + 10 + 10");
+console.log("10" + 10 + 10);
+console.log("10 + 10 + \"10\"");
+console.log(10 + 10 + "10");
+
+console.log("10 == \"10\"");
+console.log(10 == "10");
+console.log("10 === \"10\"");
+console.log(10 === "10");
+
+console.log("10 / \"10\"");
+console.log(10 / "10");
+
